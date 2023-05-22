c64935ce3232f0e3161fb4946aa567c45bf217ca
alizee <bocianalizee@gmail.com>
2023-05-22 09:39:50 +0200

    modifications globales

--- conditions.js
+++ conditions.js
@@ -1,74 +1,103 @@
 /* In a cinema the full tariff is 10 €, the reduced one is 8 €. 
 Write an algorithm which given a boolean indicating if the person can have a reduced tariff prints the price to pay.*/
-variable AGE=26;
-constante FULL=10;
-constante REDUCED=8;
+Algorithme Tarif
+variable :
+    nbr:integer
+constant full, reduced:integer
 
+
+Début
+Ecrire ("Entrer votre age")
+Lire (nbr)
 if ($age<18) then {
     output ("Reduced tariff") 
 } else {
     output ("Full tarif")
 }
+end
+
 
 //Write an algorithm which given 3 numbers finds the maximum. Integer et float//
+Algorithme Maximum
+Variable :
+    nbr:integer
 
-variable a=542;
-variable b=492;
-variable c=207;
+Début
+    Ecrire ("Entrer trois nombres")
+    Lire ($a, $b, $c)
 
 if ($a>$b) && ($a>$c) then {
     output ("The maximum is" +$a)
+    end
 } else if ($b>$a) && ($b>$c) then {
     output ("The maximum is" +$b)
+    end
 } else if ($c>$a) && ($c>$b) then {
     output ("The maximum is" +$c)
+    end
 }
+end 
+
 
 //Write an algorithm which throws 3 dices and finds out the number of identical value, three, two or none//
-integer d1=5
-integer d2=3
-integer d3=5
+Algorithme Dés
+variables:
+    d:integer
+
+Début
+    Ecrire ("Entrer les trois valeurs:")
+    Lire ($d1,$d2,$d3)
 
 if ($d1=$d2) || ($d1=$d3) || ($d2=$d3) then {
     output ("two")
+    end
 } else if ($d1=$d2) && ($d2=$d3) then {
     output ("three")
+    end
 } else {
     output ("none")
+    end
 }
+end
 
 /* Write an algorithm which given the number of a day returns its name.*/
-constant lundi=1;
-constant mardi=2;
-constant mercredi=3;
-constant jeudi=4;
-constant vendredi=5;
-constant samedi=6;
-constant dimanche=7;
+Algorithme Jours
+Variables :
+    chf:integer
 
-if ($lundi) then {
-    output(1);
-}else if ($mardi) then {
-    output(2;)
-}else if ($mercredi) then {
-    output (3);
-}else if ($jeudi) then {
-    output (4);
-}else if ($vendredi) then {
-    output (5);
-}else if ($samedi) then {
-    output (6);
-}else if ($dimanche) then {
-    output (7);
-}
+Début
+    Write("Entrer un chiffre de 1 a 7: ")
+    Lire($chf)
+    switch 
+        case 1:Ecrire("Lundi")
+        case 2:Ecrire("Mardi")
+        case 3:Ecrire("Mercredi")
+        case 4:Ecrire("Jeudi")
+        case 5:Ecrire("Vendredi")
+        case 6:Ecrire("Samedi")
+        case 7:Ecrire("Dimanche")
+        default:Ecrire("Erreur")
+    end
+
+
 
 /* A print shop charges 0.12 € the ten first copy, 0.11 € the next 20 and 0.10 € from there. Write an algorithm which given a number of copies and calculates the price.*/
-integer copy=100;
+Algorithme copy
+Variable :
+    copy:integer
+
+Début 
+Ecrire ('Entrer le nombre de copies: ')
+Lire ($copy)
 
 if ($copy<10) then {
     output (10*0.12+"€");
+    end
 } else if (10<$copy<30) then {
     output (20*0.11+"€")
+    end
 } else {
     output (0.10*$copy+"€")
+    end
 }
+end
