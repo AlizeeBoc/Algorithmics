da71d723c584feeeec83c4d6a45bcdfa1e822366
alizee <bocianalizee@gmail.com>
2023-05-22 07:19:18 +0200

    Add loops part1

---
+++ loops.js
@@ -0,0 +1,24 @@
+/* Write an algorithms which receives an integers n and prints:
+
+the numbers from 1 to n
+
+the numbers from 1 to n in descending order
+
+the numbers from -n to n
+
+the odd numbers from 1 to n */
+
+
+for (I=1; I<=n; ++I);
+    print(I);
+
+for (I=1; I<=n;--I);
+    print(I)
+
+for (-n<=I<=n;++I);
+    print(I);
+
+for (I=1;I<=n;++I)
+    if($I%2!==o) {
+        output(I)
+    }
